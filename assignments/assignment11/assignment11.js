657776a8ed420de2b2c5115bf79ffc563e87c58d
abbyhakewill <97639874+abbyhakewill@users.noreply.github.com>
2022-11-06 23:13:21 -0700

    test twitter button

--- assignments/assignment11/assignment11.js
+++ assignments/assignment11/assignment11.js
@@ -12,7 +12,7 @@ async function getQuote() {
       alert('Failed to get quote');
     }
   }
-
+/* set endpoint for quote generator api*/
 const endpoint = "https://zenquotes.io/api/quotes/";
 
 const newQuoteButton = document.querySelector('#js-new-quote');
@@ -22,10 +22,29 @@ function displayQuote(quote) {
     const quoteText = document.querySelector('#js-quote-text');
     quoteText.textContent = quote;
   }
+  getQuote();
+
+/* twitter button api
+credit to https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites for twitter api code*/
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+  
+    return t;
+  }(document, "script", "twitter-wjs"));
 
 /*function tweetQuote(quote) {
     twitterBtn.addEventListener('click', tweetQuote);
     twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
     window.open(twitterUrl, "_blank");
   }*/
- getQuote();
