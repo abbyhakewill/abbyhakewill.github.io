6c52b08ed4157fb1eeb15c8f458751ffb5bcbb15
abbyhakewill <97639874+abbyhakewill@users.noreply.github.com>
2022-11-08 11:58:27 -0700

    wa12

--- wa/wa12.js
+++ wa/wa12.js
@@ -1,49 +1,51 @@
-const minusButton = document.querySelector('.minus-button').addEventListener('click', check);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', check);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-const output = document.querySelector('.output');
-const outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-function check (){
+function check() {
     console.log('test');
 }
 
-function submit(){
+function submit() {
     alert(output.textContent);
 }
 
-function reset(){
-    const resetvalue = 0000000000;
-    output.textContent = 'resetvalue';
+function reset() {
+    outputInt = 0;
+    output.textContent = outputInt;
 }
 
-function minus(){
+function minus() {
     if (outputInt > 0) {
-    outputInt = -=1;
-    output.TextContent = outputInt; }
-
-}
-
-function plusRunner() {
-    plus(outputInt);
+    outputInt -=1;
+    output.textContent = outputInt; }
+    
 }
 
 function plus() {
     if (outputInt < 9999999999) {
     outputInt +=1;
-    output.textContent = outputInt; }
+    output.textContent = outputInt;
+    }
 }
 
 function random() {
-    outputInt.textContent = outputInt;
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
 }
 
-function randomNunmber(min, max) {
-const num = Math.floor(Math.randoom(max-m) 
-}
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+  }
+
+
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
 
 
 
@@ -57,3 +59,13 @@ button.addEventListener('click', updateOutput);
 function updateOutput() {
     output.textContent = phone_content.value;
 } */
+
+var slider = document.getElementById("myRange");
+var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
+var sliderOutput = document.querySelector(".slider-output");
+
+
+// Update the current slider value (each time you drag the slider handle)
+function update() {
+  sliderOutput.textContent = slider.value;
+}
