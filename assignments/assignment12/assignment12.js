7942b9120b1af4eb9fc00dcc61cc2578273749a9
abbyhakewill <97639874+abbyhakewill@users.noreply.github.com>
2022-11-15 11:09:39 -0700

    assign12 pt2

--- assignments/assignment12/assignment12.js
+++ assignments/assignment12/assignment12.js
@@ -5,8 +5,8 @@ var current = 5555555555;
 
 $(document).ready(function() {
   
-  $('#smaller').click(smaller);
-  $('#bigger').click(bigger);
+  $('#lower').click(lower);
+  $('#higher').click(higher);
   
   $('#submit').click(submit);
   
@@ -33,88 +33,14 @@ function updateDisplay() {
   $("#phone-number").html(numberToString(current)); 
 }
 
-function smaller() {
+function lower() {
   upper = current - 1; 
   updateDisplay(); 
 }
 
-function bigger() {
+function higher() {
   lower = current + 1; 
   updateDisplay(); 
 }
 
-
-
-
-
-
-function check() {
-    console.log('test');
-}
-
-function submit() {
-    alert(output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 9999999999) {
-    outputInt +=1;
-    output.textContent = outputInt;
-    }
-}
-
-function random() {
-    outputInt = randomNumber(0, 9999999999);
-    output.textContent = outputInt;
-}
-
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-  }
-
-
-
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
-
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
+/* code help & outline credit goes to https://blog.codepen.io/2016/11/03/ridiculous-ways-enter-phone-number-form/ */
